4d92b77ce39f6af1d60ce98d2e534e158a4386e8
Vladimir Pinchuk <vpinchuk@sandiego.edu>
2021-10-08 22:00:12 -0700

    SQL update

--- create_tables.sql
+++ create_tables.sql
@@ -6,7 +6,6 @@ CREATE TABLE stock (
     is_etf BOOLEAN NOT NULL
 );
 
-
 CREATE TABLE mention (
     stock_id INTEGER,
     dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
@@ -19,18 +18,3 @@ CREATE TABLE mention (
 
 CREATE INDEX ON mention (stock_id, dt DESC);
 SELECT create_hypertable('mention', 'dt');
-
-CREATE TABLE stock_price (
-    stock_id INTEGER NOT NULL,
-    dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    open NUMERIC NOT NULL, 
-    high NUMERIC NOT NULL,
-    low NUMERIC NOT NULL,
-    close NUMERIC NOT NULL, 
-    volume NUMERIC NOT NULL,
-    PRIMARY KEY (stock_id, dt),
-    CONSTRAINT fk_stock FOREIGN KEY (stock_id) REFERENCES stock (id)
-);
-
-CREATE INDEX ON stock_price (stock_id, dt DESC);
-SELECT create_hypertable('stock_price', 'dt');
